d27571ed2f9104e414ec6f146eeeaf2dcaea8a17
tigawanna <denniskinuthiaw@gmail.com>
2022-07-14 17:48:54 +0300

    print to visualise

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,6 +10,11 @@ for (i = 0; i < size; i++)
 {
   for (j = 0; j < size -1; j++)
   {
+    printf(" main loop %lu  ",i);
+    printf(" child loop %lu\n",j);
+    if(j ==0){
+          printf("=======level: %lu  ========\n",i);  
+    }
     if(array[j] > array[j + 1]){
         int temp=array[j];
         array[j] = array[j + 1];
